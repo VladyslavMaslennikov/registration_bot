a05dc23269eda60484c9c252662b9052b2aefff1
VladyslavMaslennykov <vladyslav.maslennikov@quantag-it.com>
2021-03-02 14:45:26 +0200

    bot new address setup

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
 #  bot token
-BOT_TOKEN = "1609459657:AAFUqxgQTpyiMvPVne60GGGNzUNaoZlsrLc"
+BOT_TOKEN = "1588532442:AAEaty_0_VZ46KmZWcxLDut27buouC_Vtao"
 # /Users/vladyslav/Desktop/Bots/First_Bot/venv/bin/python3 - директория
 GOOGLE_CLIENT_ID = "781626051596-4l7jui2l5i9068ujuuk44ujgb39k7tmf.apps.googleusercontent.com"
 GOOGLE_CLIENT_SECRET = "MIkLFb4ODBg0zqBX7mVql2I7"
-GOOGLE_CALENDAR_NAME = "Telegram Bot Register"
-GOOGLE_CALENDAR_ID: str = "eqf9elabpkubogd2buv4mvmqeg@group.calendar.google.com"
+GOOGLE_CALENDAR_NAME = "Odonatum Tattoo Bot"
+GOOGLE_CALENDAR_ID: str = "odonatum.tatoo.bot@gmail.com"
 
 my_chat_id = 270518430
 admins = [my_chat_id]
